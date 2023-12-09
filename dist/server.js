c56763d1f33c02033569fd8e29d7ada99f63d027
samsul kobir <makkobir79@gmail.com>
2023-12-09 02:20:34 +0200

    tesing for deployment

--- dist/server.js
+++ dist/server.js
@@ -1,14 +1,16 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3001;
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World How are you!");
 });
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
