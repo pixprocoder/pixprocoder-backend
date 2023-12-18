8e8f6a5a546f57b9ee79ecf63e19d277d346f5d6
samsul kobir <makkobir79@gmail.com>
2023-12-18 18:51:46 +0200

    vercel configrtion file changed

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,9 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use("/api/v1", routes_1.default);
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
 //handle not found
 app.use((req, res, next) => {
     res.status(404).json({
